ebe9f693006c7f237b402f6f3262f8ab06d939e1
Ruonan Y <joyceyang0730@gmail.com>
2020-04-04 23:32:00 -0700

    rename

--- models/index.js
+++ models/index.js
@@ -1,5 +1,60 @@
-// Exporting an object containing all of our models
+const mongoose = require("mongoose");
 
-module.exports = {
-  Workout: require("./workout")
-};
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema(
+  {
+    date: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration in minutes"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      // include any virtual properties when data is requested
+      virtuals: true
+    }
+  }
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+  var totalDuration = this.exercises.reduce(function(a,b){
+    return a + b.duration
+  }, 0);
+  // console.log(totalDuration);
+  return totalDuration;
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
