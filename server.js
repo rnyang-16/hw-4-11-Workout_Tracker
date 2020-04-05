261d93b5aa89a5bc40816495a07e25e17b5120b5
Ruonan Y <joyceyang0730@gmail.com>
2020-04-04 23:12:10 -0700

    deployed to heroku

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 var path = require("path");
 
+//If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+//Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
+
 const PORT = process.env.PORT || 3000;;
 
 const app = express();
